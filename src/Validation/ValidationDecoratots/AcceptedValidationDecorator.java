3f0da2db63b788ad78a3994574d944d155bba892
Osman <hassan.osman@divine-it.net>
2019-11-28 10:58:35 +0600

    bail implementation

--- src/Validation/ValidationDecoratots/AcceptedValidationDecorator.java
+++ src/Validation/ValidationDecoratots/AcceptedValidationDecorator.java
@@ -10,20 +10,32 @@ public class AcceptedValidationDecorator<T> extends ValidationDecorator<T> {
     public String validate(T o, String subjectFieldName) throws Exception {
 
         String displayName = getDisplayNameFormFieldName(subjectFieldName);
-
-        String validationPassedString = validationDecorator.validate(o, subjectFieldName);
-
-        String validationFailedString = displayName + " must be accepted. " + validationPassedString;
+        boolean isValidationFailed = false;
 
         if (!getIsNull()) {
 
             String value = getFieldValue(o, subjectFieldName).trim().toLowerCase();
 
             if (value.equals("1") || value.equals("yes") || value.equals("on") || value.equals("true")) {
-                return validationPassedString;
+
             }
-            return validationFailedString;
+            else{
+                isValidationFailed = true;
+            }
+
         }
+
+        boolean isFailed = isValidationFailed && isBail;
+        String message = displayName +" must be accepted. ";
+
+        if(isFailed)
+            return message;
+
+        String validationPassedString = validationDecorator.validate(o, subjectFieldName);
+        String validationFailedString = message + validationPassedString;
+
+        if(isValidationFailed)
+            return validationFailedString;
 
         return validationPassedString;
     }
