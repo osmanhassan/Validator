b4ae5f0819db351052d7d223d51be895538248a5
Osman <hassan.osman@divine-it.net>
2019-12-07 17:30:39 +0600

    validator builder

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import Pojo.User;
+import Validation.Settings.ValidatorCustomSettings;
 import Validation.Validator.Validator;
-import javafx.print.Collation;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 
 public class Main {
 
@@ -20,8 +16,11 @@ public class Main {
         user.setName("Nadim");
         user.setEmail("true");
 
-        Validator validator = new Validator(user, rulesByFields);
-        System.out.println(validator.validate());
+       String message = new Validator(user, rulesByFields)
+                            .settings(new ValidatorCustomSettings())
+                            .validate();
+       
+        System.out.println(message);
 
 
     }
