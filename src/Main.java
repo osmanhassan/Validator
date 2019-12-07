0f802bf6144d8445579f9cc8773edf17d34c2607
Osman <hassan.osman@divine-it.net>
2019-12-07 17:30:39 +0600

    builder for validator

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 import Pojo.User;
 import Validation.Settings.ValidatorCustomSettings;
 import Validation.Validator.Validator;
-import javafx.print.Collation;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 
 public class Main {
 
@@ -16,12 +11,15 @@ public class Main {
         LinkedHashMap rulesByFields = new LinkedHashMap();
         rulesByFields.put("name", "bail | date_after: 12/03/2019, MM/dd/yyyy");
         User user = new User();
-        user.setName("12/04/2019");
+
+        user.setName("12/02/2019");
         user.setEmail("has@g.cp");
 
-        Validator validator = new Validator(user, rulesByFields, new ValidatorCustomSettings());
-        System.out.println(validator.validate());
-
+       String message = new Validator(user, rulesByFields)
+                            .settings(new ValidatorCustomSettings())
+                            .validate();
+       
+        System.out.println(message);
 
     }
 }
---
+++ src/Validation/Validator/ValidationExecutor.java
@@ -0,0 +1,90 @@
+package Validation.Validator;
+
+import Validation.ValidationDecoratots.DefaultValidationDecorator;
+import Validation.ValidationDecoratots.ValidationDecorator;
+
+import java.lang.reflect.Constructor;
+import java.util.*;
+
+public class ValidationExecutor<T> {
+
+    HashMap<String, Class> validatorClassRegistry;
+    T subjectObject;
+    LinkedHashMap rulesByField;
+    HashMap settings;
+    HashMap errorMessages;
+
+    public ValidationExecutor(Validator<T> validator){
+
+        this.validatorClassRegistry = validator.validatorClassRegistry;
+        this.subjectObject = validator.subjectObject;
+        this.rulesByField = validator.rulesByField;
+        this.settings = validator.settings;
+        this.errorMessages = validator.errorMessages;
+    }
+
+    public String validate() throws IllegalAccessException {
+
+        LinkedHashMap rulesByField = this.rulesByField;
+
+        final String[] errorMessage = {""};
+
+        rulesByField.forEach((key, value) -> {
+            String fieldName = key.toString();
+            String rulesString = value.toString();
+            rulesString = "default | " + rulesString;
+            String[] rules = rulesString.split("\\|");
+
+            try {
+                errorMessage[0] += validateSingleField(fieldName, rules);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        return errorMessage[0];
+
+    }
+
+    public String validateSingleField(String subjectFieldName, String[] rules) throws Exception {
+
+        Class[] validatorClasses = new Class[rules.length];
+        String[] additionalDataOfRules = new String[rules.length];
+
+        int count = 0;
+        for(String rule : rules){
+            rule = rule.trim();
+            String[] partsOfRule = rule.split("\\:");
+            String ruleName = partsOfRule[0].trim().toLowerCase();
+
+            String additionalDataOfRule = "";
+
+            if(partsOfRule.length > 1) {
+                additionalDataOfRule = partsOfRule[1];
+            }
+
+            String className = this.validatorClassRegistry.get(ruleName).getName();
+            Class validatorClass = Class.forName(className);
+            validatorClasses[count] = validatorClass;
+            additionalDataOfRules[count] = additionalDataOfRule.trim();
+
+            count++;
+        }
+
+        ValidationDecorator<T> validationDecorator = new DefaultValidationDecorator<T>(null, "", this.settings);
+
+        for(int i = validatorClasses.length - 1; i >= 0; i --){
+
+            Class cls = validatorClasses[i];
+            String additionalDataOfRule = additionalDataOfRules[i];
+            Constructor constructor = cls.getConstructor(ValidationDecorator.class, String.class);
+            validationDecorator = (ValidationDecorator<T>) constructor.newInstance(validationDecorator, additionalDataOfRule);
+        }
+
+
+        return validationDecorator.validate(this.subjectObject, subjectFieldName);
+    }
+
+}
