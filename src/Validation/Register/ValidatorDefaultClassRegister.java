45f73133b1718c9c4211b59ee232cae36d960135
Osman <hassan.osman@divine-it.net>
2019-12-09 10:55:09 +0600

    class registry is changed

--- src/Validation/Register/ValidatorDefaultClassRegister.java
+++ src/Validation/Register/ValidatorDefaultClassRegister.java
@@ -10,21 +10,13 @@ public class ValidatorDefaultClassRegister implements IValidatorClassRegister{
         HashMap<String, Class> validatorClassRegistry = new HashMap();
 
         validatorClassRegistry
-                .put("accepted", new AcceptedValidationDecorator<>
-                        (new DefaultValidationDecorator(), "")
-                        .getClass());
+                .put("accepted", AcceptedValidationDecorator.class);
 
         validatorClassRegistry
-                .put("default",
-                        new DefaultValidationDecorator<>
-                                (new DefaultValidationDecorator(), "")
-                                .getClass());
+                .put("default", DefaultValidationDecorator.class);
 
         validatorClassRegistry
-                .put("bail",
-                        new BailValidationDecorator<>
-                                (new DefaultValidationDecorator(), "")
-                                .getClass());
+                .put("bail", BailValidationDecorator.class);
 
         return validatorClassRegistry;
     }
