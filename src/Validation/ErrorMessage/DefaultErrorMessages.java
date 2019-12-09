e0acd4eb4f4769f7cafbd43c063935775d9fc4eb
Osman <hassan.osman@divine-it.net>
2019-12-09 14:39:24 +0600

    bail and error message are handled from outside of concrete validation decorator

---
+++ src/Validation/ErrorMessage/DefaultErrorMessages.java
@@ -0,0 +1,15 @@
+package Validation.ErrorMessage;
+
+import java.util.HashMap;
+
+public class DefaultErrorMessages implements IErrorMessages {
+    @Override
+    public HashMap<String, String> getErrorMessages() {
+        HashMap<String, String> errorMessages = new HashMap<>();
+
+        errorMessages.put("default", "");
+        errorMessages.put("bail", "");
+
+        return errorMessages;
+    }
+}
